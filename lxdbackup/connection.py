76aa4993e94c1f4ae4692c4713ec8819b0200199
Jon Clayton <m87tech@gmail.com>
2021-06-19 13:36:49 +0000

    trying to get authentication working

--- lxdbackup/connection.py
+++ lxdbackup/connection.py
@@ -1,22 +1,27 @@
 from typing import Any, Optional
 import paramiko
-from pydantic import basemodel
+from pydantic import BaseModel
 from paramiko import channel, client
 
-class ConnectArgs(basemodel):
+
+class ConnectArgs(BaseModel):
     hostname: str = None
     username: str = None
     password: str = None
-    
-    def __init__(self, hostname: str, username: str, password: str) -> None:
-        self.hostname = hostname
-        self.username = username
-        self.password = password
-    
 
-class FactoryConnection():
 
-    def get_paramiko_connection():
-        conn = client.SSHClient.connect(hostname="blah", port="foo")
+class FactoryConnection:
+    @staticmethod
+    def get_paramiko_connection(connect_args: ConnectArgs):
+        c = client.SSHClient()
+        c.load_system_host_keys()
+        c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        c.connect(hostname=connect_args.hostname)
+        return c
 
-class Connection(connect_args: ConnectArgs, conn: Any)
+
+class Connection:
+    def __init__(self, conn: client.SSHClient) -> None:
+        self.conn = conn
+
+        pass
--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from lxdbackup import backup, connection
 
 def main():
     print("do something")
+    connect_args = connection.ConnectArgs(
+        hostname="10.55.0.66", username="root", password="bailey"
+    )
+    paramiko_conn = connection.FactoryConnection.get_paramiko_connection(connect_args)
+    conn = connection.Connection(conn=paramiko_conn)
 
 
-if "__name__" == "main":
+if __name__ == "__main__":
     main()
