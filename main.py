f42682d9ae4542ba803874c528853a3ebed4cd2e
Jon Clayton <m87tech@gmail.com>
2021-06-19 14:39:43 +0000

    refactor and tidyup

--- main.py
+++ main.py
@@ -2,14 +2,27 @@ from pydantic.main import BaseConfig
 from lxdbackup import backup, connection
 from config.base_config import BaseConfig
 
+FILENAME = ".creds.yml"
+
 
 def main():
-    creds = BaseConfig.get_creds(filename=".creds.yml")
+    username, password = get_credentials()
+    connect_args = get_connect_args(username, password)
+    paramiko = connection.FactoryConnection.get_paramiko_connection(connect_args)
+    conn = connection.Connection(conn=paramiko)
+
+
+def get_connect_args(username, password):
     connect_args = connection.ConnectArgs(
-        hostname="10.55.0.66", username="root", password=creds
+        hostname="10.55.0.66", username=username, password=password
     )
-    paramiko_conn = connection.FactoryConnection.get_paramiko_connection(connect_args)
-    conn = connection.Connection(conn=paramiko_conn)
+    return connect_args
+
+
+def get_credentials():
+    username = BaseConfig.get_username(filename=FILENAME)
+    password = BaseConfig.get_password(filename=FILENAME)
+    return username, password
 
 
 if __name__ == "__main__":
