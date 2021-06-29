163958f64b6f6bda0820e55dedcdf7b79053f9f9
Jon Clayton <m87tech@gmail.com>
2021-06-29 21:08:28 +0000

    passing test

--- tests/test_baseconfig.py
+++ tests/test_baseconfig.py
@@ -10,10 +10,20 @@ import confuse
 from pytest_cases import fixture
 
 
-def test_get_endpoint_url(self):
-    prefix = "https://"
-    host = self.config["lxd"]["api"][0]["endpoint"].get()
-    return f"{prefix}{host}"
-    
-def test_get_auth(self, auth_type):
-    return str(self.config["lxd"]["api"][0]["auth"][auth_type])
+@pytest.fixture(autouse=True)
+def config() -> confuse.Configuration:
+    config = confuse.Configuration("lxdbackup", __name__)
+    config.set_file(".config.yml")
+    return config
+
+
+def test_set_endpoint_host(config):
+    host = config["lxd"]["api"][0]["endpoint"].get()
+    assert host == "10.55.0.21:8443"
+
+    """def get_endpoint_url(self):
+        return f"{self.prefix}{self.host}"
+
+    def get_auth(self, auth_type):
+        return str(self.config["lxd"]["api"][0]["auth"][auth_type])
+"""
